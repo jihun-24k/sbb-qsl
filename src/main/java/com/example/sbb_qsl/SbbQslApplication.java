43832ba5c9e8e508376989ce85c782d65521028b
jihun-24k <gi020477@gmail.com>
2022-08-27 08:30:33 +0900

    Chore: 세팅

---
+++ src/main/java/com/example/sbb_qsl/SbbQslApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbb_qsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbQslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbQslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbb_qsl/SbbQslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbb_qsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbQslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
