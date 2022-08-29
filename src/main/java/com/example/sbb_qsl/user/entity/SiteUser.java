ff57a457147a4400d336955c267f013c54c1b398
jihun-24k <gi020477@gmail.com>
2022-08-29 14:55:25 +0900

    Refactor: builder 도입

--- src/main/java/com/example/sbb_qsl/user/entity/SiteUser.java
+++ src/main/java/com/example/sbb_qsl/user/entity/SiteUser.java
@@ -1,15 +1,12 @@
 package com.example.sbb_qsl.user.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
-@Setter
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SiteUser {
